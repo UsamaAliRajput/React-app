9761de5405189eda58959ca883016fc5018b2191
Usama Ali <usama.bscsf19@iba-suk.edu.pk>
2023-11-01 21:28:14 +0500

    2nd commit

---
+++ src/GlobalComponent.js
@@ -0,0 +1,12 @@
+// src/GlobalComponent.js
+import React from 'react';
+
+function GlobalComponent() {
+  return (
+    <div>
+      This is a global component.
+    </div>
+  );
+}
+
+export default GlobalComponent;
